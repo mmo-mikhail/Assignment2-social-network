390a8a06959ef510e7c58a8bf9c99c337539859a
s3419069 (Mykhailo Muzyka) <s3419069 (Mykhailo Muzyka)@DESKTOP-A3V7VF1.home>
2018-05-15 13:08:04 +1000

    update db initializer and repository

---
+++ Assignment2/src/repository/Initializer.java
@@ -0,0 +1,363 @@
+/*
+ * @author s3419069 (Mykhailo Muzyka)
+ * 
+ * Copyright (c) 2018 RMIT University, Advanced Programming (COSC1295) Assignment 2
+ */
+package repository;
+
+import java.io.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.hsqldb.Server;
+
+public class Initializer {
+
+	public final static String dbName = "jdbc:hsqldb:db\\socialNetwork;hsqldb.write_delay=false;";
+	public final static String userName = "SA";
+	public final static String userPass = "123";
+	
+	static String logs = "";
+	
+	// The name of the people file to open.
+	final static String peopleFileName = "people.txt";
+
+	// The name of the relationship file to open.
+	final static String relationshipsFileName = "relations.txt";
+
+	/**
+	 * storage of profiles
+	 */
+	private static ProfileRepository profileRepository =
+			new ProfileRepository();
+	
+	private Initializer() {
+	}
+
+	public static String Init() {
+		if (!checkClassPath()) {
+			return "DB Error: Try add hsqldb.jar to classpath";
+		}
+		File peopleFile = new File(peopleFileName);
+		System.out.println(peopleFile.getAbsolutePath());
+		File relationFile = new File(relationshipsFileName);
+		if (peopleFile.exists() && !peopleFile.isDirectory()
+				&& relationFile.exists() && !relationFile.isDirectory()) {
+			// try create/udpate database
+			return updateDb();
+		}
+		// if db exist, it will be used later. Otherwise display error
+		if (!isDbExists()) {
+			return "Error: required files and database cannot be found!";
+		}
+		return null;
+	}
+
+	private static boolean checkClassPath() {
+		try {
+			Class.forName("org.hsqldb.jdbc.JDBCDriver");
+		} catch (java.lang.ClassNotFoundException e) {
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean isDbExists() {
+		try {
+			Connection con =
+					DriverManager.getConnection(
+							dbName + ";ifexists=true", userName, userPass);
+			Statement stmt = con.createStatement();
+		} catch (Exception e) {
+			if (e.getMessage().contains("Database does not exists")) {
+				return false;
+			}
+			e.printStackTrace(System.out);
+		}
+		return true;
+	}
+
+	private static String updateDb() {
+		try {
+			Connection con =
+					DriverManager.getConnection(dbName, userName, userPass);
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(
+					"DROP TABLE IF EXISTS Relations;"
+					+ "DROP TABLE IF EXISTS Profiles;"
+					+ "CREATE TABLE Profiles (\r\n"
+					+ "Name VARCHAR(100) NOT NULL,\r\n"
+					+ "Image VARCHAR(100) NOT NULL,\r\n"
+					+ "Status VARCHAR(100) NOT NULL,\r\n"
+					+ "Gender VARCHAR(100) NOT NULL,\r\n"
+					+ "Age VARCHAR(100) NOT NULL,\r\n"
+					+ "State VARCHAR(100) NOT NULL,\r\n"
+					+ "PRIMARY KEY (Name));\r\n"
+					
+					+ "CREATE TABLE Relations (\r\n"
+					+ "FirstProfile VARCHAR(100) NOT NULL,\r\n"
+					+ "SecondProfile VARCHAR(100) NOT NULL,\r\n"
+					+ "Relation VARCHAR(50) NOT NULL,\r\n"
+					+ "Primary key(firstProfile, secondProfile, relation),\r\n"
+					+ "Foreign key(firstProfile) references "
+					+ "public.Profiles(Name),\r\n"
+					+ "Foreign key(secondProfile) references "
+					+ "public.Profiles(Name));");
+			if (!readPeopleFile(stmt)) {
+				return "Unexpected Error: 'people.txt' file is corrupted";
+			}
+			if (!readRelashionsFile(stmt)) {
+				return "Unexpected Error: 'relations.txt' file is corrupted";
+			}
+			deleteWrongProfiles(stmt);
+			con.commit();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+			return "DB Error: Cannot Access Database!";
+		}
+		return null;
+	}
+
+	private static void deleteWrongProfiles(Statement stmt)
+			throws SQLException {
+		
+		//simple query below retrieves names of children who don't have two parents. 
+		ResultSet rs = stmt.executeQuery(
+				"select Name from public.Profiles \r\n" + 
+				"where age < 16 and (\r\n" + 
+				" 2 <> (select count(*) from public.Relations where "
+				+ "relation = 'parent' and FirstProfile = Profiles.Name) "
+				+ "and\r\n" + 
+				" 2 <> (select count(*) from public.Relations "
+				+ "where relation ='parent' and SecondProfile =Profiles.Name) "
+				+ "and\r\n" + 
+				" ( 1 <> (select count(*) from public.Relations "
+				+ "where relation = 'parent' and FirstProfile =Profiles.Name) "
+				+ "and\r\n" + 
+				"   1 <> (select count(*) from public.Relations "
+				+ "where relation = 'parent' "
+				+ "and SecondProfile = Profiles.Name) ) )");
+		List<String> profilesDeleted = new ArrayList<String>();
+		while(rs.next()) {
+			String name = rs.getString("Name");
+			profileRepository.delete(name);
+			profilesDeleted.add(name);
+		}
+		if (profilesDeleted.size() == 0) return;
+		addLogs(System.lineSeparator() + "Profiles deleted due to constrains:"
+				+ System.lineSeparator()
+				+ String.join(System.lineSeparator(), profilesDeleted));
+	}
+
+	private static boolean readRelashionsFile(Statement stmt) {
+		try {
+			// FileReader reads text files in the default encoding.
+			// and wrapping FileReader in BufferedReader.
+			FileReader fileReader = new FileReader(relationshipsFileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] values = line.split(",");	
+				if (values.length != 3 || line.contains("'")) {
+					addLogs("relations.txt content warning: " + values + 
+							" - invalid row!");
+					continue;
+				}
+				String name1 =values[0].trim();
+				String name2 =values[1].trim();
+				String relation = values[2].trim();
+				if (!isNameExist(name1, stmt) || !isNameExist(name2, stmt)
+					|| !isValidRelation(values, stmt, line)) {
+					continue;
+				}
+				profileRepository.setRelation(name1, name2, relation, stmt);
+			}
+			// Always close files.
+			bufferedReader.close();
+		} catch(Exception e) {
+			e.printStackTrace(System.out);
+			// System.out.println(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean isValidRelation(String[] values,
+			Statement stmt, String line)
+			throws SQLException {
+		String name1 =values[0].trim();
+		String name2 =values[1].trim();
+		String warningLine = "relations.txt content warning: " + line + " - ";
+		if (name1.equals(name2)) {
+			addLogs(warningLine + "same names");
+			return false;
+		}
+		int age1 = getAge(name1, stmt);
+		int age2 = getAge(name2, stmt);
+		switch(values[2].trim()) {
+			case "friends":
+				if (age1 <= 16 && age2 > 16 || age2 <= 16 && age1 > 16) {
+					addLogs(warningLine
+							+ "invalid friendship child with adult");
+					return false;
+				}
+				if (age1 <= 2 || age2 <= 2) {
+					addLogs(warningLine + "young child cannot have friends");
+					return false;
+				}
+				if (age1 <= 16 && age2 <= 16
+					&& (age1 - age2 > 3 || age2 - age1 > 3)) {
+					addLogs(warningLine + "child's age diff is over 3 years");
+					return false;
+				}
+				break;
+			case "parent":
+				if (age1 <= 16 && age2 <= 16) {
+					addLogs(warningLine + "parents are too young");
+					return false;
+				}
+				if (age1 > 16 && age2 > 16) {
+					addLogs(warningLine + "child is too old");
+					return false;
+				}
+				break;
+			case "classmates":
+				if (age1 <= 2 || age2 <= 2) {
+					addLogs(warningLine + "young child cannot be classmates");
+					return false;
+				}
+				break;
+			case "colleagues":
+				if (age1 <= 16 || age2 <= 16) {
+					addLogs(warningLine + "only adults can be colleagues");
+					return false;
+				}
+				break;
+			case "couple":
+				if (age1 <= 16 || age2 <= 16) {
+					addLogs(warningLine + "only adults can be married");
+					return false;
+				}
+				//All couples are mutually exclusive to other couples
+				if (!profileRepository.coupleAllowed(name1, name2, stmt)) {
+					addLogs(warningLine + "all couples are mutually exclusive"
+							+ " to other couples");
+					return false;
+				}
+				break;
+		}
+		return true;
+	}
+
+	
+
+	private static int getAge(String name, Statement stmt)
+			throws SQLException {
+		ResultSet rs = stmt.executeQuery(
+				"select Age from public.Profiles where Name ='"
+				+ name + "'");
+		rs.next();
+		return rs.getInt(1);
+	}
+
+	private static boolean isNameExist(String name, Statement stmt)
+			throws SQLException {
+		ResultSet rs = stmt.executeQuery(
+				"select count(*) from public.Profiles where Name ='"
+				+ name + "'");
+		if (rs.next() && rs.getInt(1) == 0) {
+			addLogs("relations.txt content warning: " + name + 
+					" - no such name in people.txt!");
+			return false;
+		}
+		return true;
+	}
+
+	private static String[] validStates =
+			new String[] {"ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC","WA"};
+	
+	private static boolean readPeopleFile(Statement stmt) {
+		try {
+			// FileReader reads text files in the default encoding.
+			// and wrapping FileReader in BufferedReader.
+			FileReader fileReader = new FileReader(peopleFileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				line = line.replace("“", "").replace("”", "")
+						.replace("\"", "");
+				String[] values = line.split(",");
+				if (!validatePerson(values)) {
+					continue;
+				}
+				profileRepository.add(
+						values[0].trim(),
+						values[1].trim(),
+						values[2].trim(),
+						values[3].trim(),
+						values[4].trim(),
+						values[5].trim(), stmt);
+			}
+			// Always close files.
+			bufferedReader.close();
+		} catch(Exception e) {
+			e.printStackTrace(System.out);
+			return false;
+		}
+		return true;
+	}
+	
+	private static boolean validatePerson(String[] values) {
+		if (values.length == 1) {
+			addLogs("people.txt content error: " + values[0]
+					+ " - must be comma separated!");
+			return false;
+		}
+		if (values.length != 6) {
+			addLogs("people.txt content warning: " + values[0]
+					+ " - has invalid number of parameters!");
+			return false;
+		}
+		if (!Arrays.asList(validStates).contains(values[5].trim())) {
+			addLogs("people.txt content warning: " + values[0]
+					+ " - has invalid state!");
+			return false;
+		}
+		if (!values[3].trim().equals("M") && !values[3].trim().equals("F")) {
+			addLogs("people.txt content warning: " + values[0]
+					+ " - has invalid gender!");
+			return false;
+		}
+		return true;
+	}
+
+	private static void addLogs(String text) {
+		logs += text + " (current line will be skipped)" + System.lineSeparator();
+	}
+	
+	public static String getLogs() {
+		return logs;
+	}
+}
+
+/*
+ CREATE TABLE Profiles ( id INT NOT NULL, Name VARCHAR(100) NOT NULL, Status
+ VARCHAR(100) NOT NULL, Image VARCHAR(100) NOT NULL, Gender VARCHAR(100) NOT
+ NULL, Age VARCHAR(100) NOT NULL, LivingState VARCHAR(100) NOT NULL, PRIMARY
+ KEY (id)); CREATE TABLE Relations ( FirstProfileId INT NOT NULL,
+ SecondProfileId INT NOT NULL, Relation VARCHAR(50) NOT NULL, Primary
+ key(firstProfileId, secondProfileId), Foreign key(firstProfileId) references
+ public.Profiles(id), Foreign key(secondProfileId) references
+public.Profiles(id));
+
+
+select Name from public.Profiles
+where age < 16 and (
+ 2 <> (select count(*) from public.Relations where relation = 'parent' and FirstProfile = Profiles.Name) and
+ 2 <> (select count(*) from public.Relations where relation = 'parent' and SecondProfile = Profiles.Name) and
+ ( 1 <> (select count(*) from public.Relations where relation = 'parent' and FirstProfile = Profiles.Name) and
+   1 <> (select count(*) from public.Relations where relation = 'parent' and SecondProfile = Profiles.Name) ) )
+*/
--- Assignment2/src/repository/ProfileRepository.java
+++ Assignment2/src/repository/ProfileRepository.java
@@ -1,89 +1,296 @@
 /*
  * @author s3419069 (Mykhailo Muzyka)
  * 
- * Copyright (c) 2018 RMIT University, Advanced Programming (COSC1295) Assignment 1 
+ * Copyright (c) 2018 RMIT University, Advanced Programming (COSC1295) Assignment 2
  */
 package repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
+import CustomExceptions.*;
 import users.*;
 
 /**
  * @author s3419069 (Mykhailo Muzyka)
- *	this class represents Data Access Layer. Here we can refer to other resources to get data such as DB or files
- *  in this case we just hardcode data
+ *	this class represents Data Access Layer. Here we can refer to other resources to get data such as DB
  */
 public class ProfileRepository {
-
-	static ArrayList<Profile> profiles; //here all profiles is stores
-	
-	private static ArrayList<Profile> getProfiles() {
-		if (profiles == null) {
-			//only the first time profiles list is called, it got initialized with hard coded data
-			profiles = new ArrayList<Profile>();
-			init();
-		}
-		return profiles;
-	}
-	
-	/**
-	 * data initialization method
-	 */
-	private static void init() {
-
-	}
 	
 	/**
 	 * return all profiles from current repository
 	 */
 	public List<Profile> getAll() {
-		return getProfiles();
+		try {
+			Connection con =
+					DriverManager.getConnection(
+							Initializer.dbName,
+							Initializer.userName, Initializer.userPass);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from Profiles");
+			List<Profile> allProfiles = new ArrayList<Profile>();
+			while (rs.next()) {
+				String name = rs.getString("Name");
+				String image = rs.getString("Image");
+				String status = rs.getString("Status");
+				String gender = rs.getString("Gender");
+				int age = rs.getInt("Age");
+				String state = rs.getString("State");
+				
+				Profile profile = null;
+				if (age > 16) {
+					profile = new AdultProfile(name, age, status, gender, state);
+					fillChildren((AdultProfile)profile, stmt);
+				}
+				else {
+					String[] parents = getParents(name);
+					AdultProfile parent1 = (AdultProfile)getProfile(parents[0]);
+					AdultProfile parent2 = (AdultProfile)getProfile(parents[1]);
+					
+					profile = new ChildProfile(name, age, status, image, gender, state,
+							parent1.getGender() == "F"
+								? parent1 : parent2,
+							parent2.getGender() == "M"
+								? parent2 : parent1);
+				}
+				allProfiles.add(profile);
+			}
+			System.out.println(allProfiles.size());
+			return allProfiles;
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+		return null;	
+	}
+	
+	public Profile getProfile(String Name) {
+		return getProfile(Name, true);
+	}
+	
+	public Profile getProfile(String Name, boolean fillChildren) {
+		try {
+			Connection con =
+					DriverManager.getConnection(
+							Initializer.dbName,
+							Initializer.userName, Initializer.userPass);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from Profiles "
+					+ "where Name ='" + Name + "'");
+			if (!rs.next()) return null;
+			
+			String name = rs.getString("Name");
+			String image = rs.getString("Image");
+			String status = rs.getString("Status");
+			String gender = rs.getString("Gender");
+			int age = rs.getInt("Age");
+			String state = rs.getString("State");
+			
+			if (age > 16) {
+				AdultProfile profile = new AdultProfile(name, age,
+						status, gender, state);
+				if (fillChildren) {
+					fillChildren(profile, stmt);
+				}
+				return profile;
+			}
+			else {
+				String[] parents = getParents(name);
+				AdultProfile parent1 = (AdultProfile)getProfile(parents[0], false);
+				AdultProfile parent2 = (AdultProfile)getProfile(parents[1], false);
+				
+				return new ChildProfile(name, age, status, image, gender,
+						state, parent1, parent2);
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+		return null;	
+	}
+	
+	
+	private void fillChildren(AdultProfile profile, Statement stmt)
+			throws SQLException {
+		ResultSet rs = stmt.executeQuery("select CASEWHEN("
+				+ "SecondProfile ='" + profile.getName() + "',"
+				+ "FirstProfile, SecondProfile) from Relations "
+				+ "where (FirstProfile = '" + profile.getName()
+				+ "' or SecondProfile = '" + profile.getName()
+				+ "') and relation = 'parent'");
+		while (rs.next()) {
+			String name = rs.getString(1);
+			profile.addChildren((ChildProfile)getProfile(name));
+		}
+	}
+
+	public String[] getParents(String Name) throws NoParentException {	
+		try {
+			Connection con =
+				DriverManager.getConnection(
+							Initializer.dbName,
+							Initializer.userName, Initializer.userPass);
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(
+				"select CASEWHEN(p.Name = r.FirstProfile,"
+				+ "r.SecondProfile, r.FirstProfile), Gender" + 
+				" from public.Profiles p\r\n" + 
+				"join relations r on"
+				+ "(r.FirstProfile = p.Name or r.SecondProfile = p.Name)"
+				+ "and relation = 'parent'\r\n" + 
+				"where age < 16 and name = '" + Name + "'");
+			if (!rs.next()) throw new NoParentException(Name + "has no parents");
+			
+			String[] parents = new String[2];
+			parents[0] = rs.getString(1);
+			if (!rs.next()) throw new NoParentException(Name + "has no parent");
+			parents[1] = rs.getString(1);
+			
+			return parents;
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+		throw new NoParentException(Name + "has no parent");
 	}
 	
 	/**
 	 * add new profile
+	 * @throws SQLException 
 	 */
-	public void add(Profile profile) {
-		if (profile != null) {
-			getProfiles().add(profile);
-		}
+	public void add(String name, String image, String status, String gender,
+				String age, String state, Statement stmt)
+			throws SQLException {
+		String query = "INSERT INTO public.Profiles VALUES ('"
+				+ name + "','" 		//Name
+				+ image + "','" 		//Image
+				+ status + "','" 	//Status
+				+ gender + "'," 	//Gender
+				+ age + ",'" 		//Age
+				+ state + "')"; 	//State
+		stmt.executeUpdate(query);
 	}
 	
 	/**
-	 * Change profile
+	 * add new profile
+	 * @throws SQLException 
 	 */
-	public void update(UUID profileId, Profile profile) {
-		//find old profile
-		Profile prof = findByGuid(profileId);
-		if (prof != null) {
-			int idx = getProfiles().indexOf(prof); //get index in list of old profile
-			getProfiles().set(idx, profile); //perform update
-		}
+	public void add(String name, String image, String status, String gender,
+			int age, String state)
+			throws SQLException {
+		Statement stmt = DriverManager.getConnection(
+				Initializer.dbName,
+				Initializer.userName, Initializer.userPass)
+				.createStatement();
+		add(name, image, status, gender, String.valueOf(age), state, stmt);
 	}
 	
 	/**
-	 * delete the profile by its Id]
-	 * return return true when profileId is found
+	 * delete the profile by its Name
+	 * return false when error occurs
 	 */
-	public Boolean delete(UUID profileId) {
-		Profile prof = findByGuid(profileId);
-		if (prof != null) {
-			//when the profile by id is found, remove it from list
-			getProfiles().remove(prof);
+	public Boolean delete(String name) {
+		try {
+			Statement stmt = DriverManager.getConnection(
+					Initializer.dbName,
+					Initializer.userName, Initializer.userPass)
+					.createStatement();
+			stmt.executeUpdate("delete from public.Relations "
+					+ "where FirstProfile ='" + name + "'");
+			stmt.executeUpdate("delete from public.Relations "
+					+ "where SecondProfile ='" + name + "'");
+			stmt.executeUpdate("delete from public.Profiles "
+					+ "where Name ='" + name + "'");				
 			return true;
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
 		}
-		//return false when profileId is not found
+		return false;
+	}
+
+	public boolean isDirectlyConnected(String name1, String name2) {
+		try {
+			Statement stmt = DriverManager.getConnection(
+					Initializer.dbName,
+					Initializer.userName, Initializer.userPass)
+					.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from Relations "
+					+ "where (FirstProfile ='" + name1 + "' and "
+					+ "SecondProfile ='" + name2 + "') or (FirstProfile ='"
+					+ name2 + "' and SecondProfile ='" + name1 + "')");
+			return rs.next();
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
 		return false;
 	}
 	
-	/**
-	 * get the profile from storage by its id
-	 */
-	private Profile findByGuid(UUID profileId) {
-		return getProfiles().stream()
-				.filter(profile -> profile.getProfileGuid() == profileId)
-				.findFirst()
-				.orElse(null);
+	public boolean coupleAllowed(String name1, String name2)
+			throws SQLException {
+		Statement stmt = DriverManager.getConnection(
+				Initializer.dbName,
+				Initializer.userName, Initializer.userPass)
+				.createStatement();
+		return coupleAllowed(name1, name2, stmt);
+	}
+	
+	public boolean coupleAllowed(String name1, String name2,
+			Statement stmt) throws SQLException {
+		// try to find another couple when at least one spouse is part
+		// of other couple
+		ResultSet rs = stmt.executeQuery( 
+				"select distinct * from relations \r\n" + 
+				"where relation = 'couple' and (\r\n" + 
+				"'" + name1 + "' = firstprofile or\r\n" + 
+				"'" + name1 + "' = secondprofile or \r\n" + 
+				"'" + name2 + "' = firstprofile or \r\n" + 
+				"'" + name2 + "' = secondprofile  )");		
+		return !rs.next(); //returns true when no element found
+	}
+
+	public boolean setRelation(String name1, String name2, String relation) {
+		try {
+			Statement stmt = DriverManager.getConnection(
+					Initializer.dbName,
+					Initializer.userName, Initializer.userPass)
+					.createStatement();
+			setRelation(name1, name2, relation, stmt);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+		return false;
+	}
+	
+	public void setRelation(String name1, String name2, String relation,
+			Statement stmt) throws SQLException {
+		stmt.executeUpdate("INSERT INTO public.Relations VALUES ('"
+				+ name1 + "','"
+				+ name2 + "','"
+				+ relation + "')");
+	}
+
+	public String[] getSpouses(String separator) {
+		try {
+			Statement stmt = DriverManager.getConnection(
+					Initializer.dbName,
+					Initializer.userName, Initializer.userPass)
+					.createStatement();
+			//select only couples
+			ResultSet rs = stmt.executeQuery(
+					"select * from relations where relation = 'couple'");
+			if (!rs.next()) return null; //when no couples found
+			List<String> couples = new ArrayList<>();
+			do {
+				couples.add(rs.getString(1) + separator + rs.getString(2));
+			} while (rs.next());
+			//convert list to array and return
+			String[] stockArr = new String[couples.size()];
+			return couples.toArray(stockArr);
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+		return null;
 	}
 }
